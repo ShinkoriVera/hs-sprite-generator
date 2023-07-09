51ff34ab86eca98d15a716d2be286dd8f70d345e
ShinkoriVera <54640787+ShinkoriVera@users.noreply.github.com>
2023-07-09 23:09:57 +0700

    Update spritegen.js

--- spritegen.js
+++ spritegen.js
@@ -1,29 +1,29 @@
-var charselection = selectchar.characters; // указываю выпадающий список
-var charlist = []; // задаю список персонажей
-for (character of charselection) {
+var selectedchar = selectchar.characters;
+var charlist = [];
+for (character of selectedchar) {
     charlist.push(character)
-}; // помещаю в список персонажей варианты выпадающего списка
+};
 
-const compareRandom = ( ) => Math.random() - 0.5; // прописываю рандом для генерации второго персонажа
-const randomInteger = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1)); // прописываю рандом для значений цвета
+const compareRandom = ( ) => Math.random() - 0.5;
+const randomInteger = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
 
 function generatepair() {
-    var choice = charselection.options[charselection.selectedIndex]; // даю знать, какой персонаж выбран пользователем в списке
-    var randchar = charlist.filter(c => c !== choice); // создаю список персонажей для рандома, исключая из него выбранного
-    randchar.sort(compareRandom); // сортирую список в случайном порядке
-    document.querySelector('#resultpair').textContent = choice.text + "+" + randchar[0].text; // вывожу текст с именем выбранного персонажа и первого в рандомном списке
+    var choice = selectedchar.options[selectedchar.selectedIndex];
+    var randchar = charlist.filter(c => c !== choice);
+    randchar.sort(compareRandom);
+    document.querySelector('#resultpair').textContent = choice.text + "+" + randchar[0].text;
 };
 
 function generatecolor() {
-    var randH = randomInteger(0,360); // случайное значение оттенка
-    var randS = randomInteger(60,100); // случайное значение насыщенности
-    var randL = randomInteger(35,70); // случайное значение светлоты
-    document.getElementById("randcolor").style.backgroundColor = "hsl(" + randH + "," + randS + "%," + randL + "%)"; // вставка рандомного цвета из значений выше
+    var randH = randomInteger(0,360);
+    var randS = randomInteger(60,100);
+    var randL = randomInteger(35,70);
+    document.getElementById("randcolor").style.backgroundColor = "hsl(" + randH + "," + randS + "%," + randL + "%)";
 };
 
 function generatesprite() {
     generatepair();
     generatecolor();
-}; //общая функция
+};
 
-document.querySelector('#startgen').addEventListener('click', generatesprite); // запуск генерации по нажатию кнопки
+document.querySelector('#startgen').addEventListener('click', generatesprite);
